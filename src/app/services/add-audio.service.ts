77830d88092864378ac36d95f89c93f103a19cec
Moshe Cohen <moshe22cohen@gmail.com>
2019-10-24 21:13:32 +0300

    still need to fix cors issue

--- src/app/services/add-audio.service.ts
+++ src/app/services/add-audio.service.ts
@@ -1,17 +1,26 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+import { debug } from 'util';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddAudioService {
 
-   private _postAudio = "localhost:4400/addFile";
+   private _postAudio = "http://localhost:4400/addFile";
 
-  constructor(private _http: HttpClient) { }
+   _headers: HttpHeaders = new HttpHeaders().append('content-type', 'multipart/form-data').append('Access-Control-Allow-Origin','*').append("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT").append("Access-Control-Allow-Headers",'Access-Control-Allow-Origin');
+  constructor(private _http: HttpClient) {
+
+   }
+
+
 
   upload($fd) {
-    this._http.post(this._postAudio, $fd)
+
+
+    this._http.post(this._postAudio, $fd,{headers:this._headers})
     .subscribe( result => {
 
       //TODO: check it is info from response
@@ -21,6 +30,15 @@ export class AddAudioService {
 
 
 
+
+  /*
+  Note:
+  i used this to solve the issue with passing user input as parameters :
+  https://stackoverflow.com/questions/39152071/cant-bind-to-formgroup-since-it-isnt-a-known-property-of-form
+
+  and this for the form and such:
+  https://stackoverflow.com/questions/49993908/angular-and-nodejs-sending-image
+  */
 
 
 
